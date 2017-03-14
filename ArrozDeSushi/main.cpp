b646a222d6248dd3b51909dad6aa95d07946f01c
Meiogordo <gamer-go@hotmail.com>
2017-03-14 09:34:24 +0000

    started copying useful functions and testing running with the new file structure

--- ArrozDeSushi/main.cpp
+++ ArrozDeSushi/main.cpp
@@ -7,3 +7,29 @@ int main() {
 
 	return 0;
 }
+
+// a pôr noutro ficheiro
+/*
+vector<string> ReadFile(string path) {
+
+	vector<string> inputstuff;
+
+	//infile is path
+
+	ifstream myfile;
+	string line;
+
+	myfile.open(path);
+
+	int counterRead = 0;
+	while (getline(myfile, line)) {
+		inputstuff.push_back(line);
+		counterRead++;
+		cout << "\na ler linha" << counterRead;
+	}
+
+	myfile.close();
+
+	return inputstuff;
+}
+*/
